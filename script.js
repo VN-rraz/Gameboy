6da89ada22ce050a528a51aaead9bac14b35759c
Vinícius Ferraz <65097335+VN-rraz@users.noreply.github.com>
2022-09-17 12:02:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function ligar(){
     document.body.style.color = 'red'
 }
-
-function a(){
+function pokemon(){
     var res = document.getElementById('res')
-
+    res.innerHTML = ''//esse código visivelmente vazio conserta um bug por incrível que pareça.
     var img = document.createElement('img')
         img.setAttribute('id','foto')
-        img.setAttribute('src','imagens/tetris.jpg')
-
-        res.appendChild(img)
+        img.setAttribute('src','imagens/pokemon yellow2.jpg')
+        res.appendChild(img)  
+}
+function mario(){
+    var res = document.getElementById('res')
+    res.innerHTML = ''//esse código visivelmente vazio conserta um bug por incrível que pareça.
+    var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        img.setAttribute('src','imagens/super mario land2.jpg')
+        res.appendChild(img)  
+}
+function tetris(){
+    var res = document.getElementById('res')
+    res.innerHTML = ''//esse código visivelmente vazio conserta um bug por incrível que pareça.
+    var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        img.setAttribute('src','imagens/tetris2.jpg')
+        res.appendChild(img)  
 }
